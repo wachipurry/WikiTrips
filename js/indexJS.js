3f4de77ffef5429038e77804a31c068d0f671091
wachipurry <jamace99@gmail.com>
2019-12-10 22:45:28 +0100

    Preparación de formularios de logIn SigIn y creación de estos dentro de alerts en boostrap

--- js/indexJS.js
+++ js/indexJS.js
@@ -4,8 +4,35 @@ class Model {
     constructor(){
         console.log("Model creat");
     }   
+    /*
+        101-> Ver la preview de las experiencias
+        102-> Experiencias completas
+        201-> Log in
+        202-> Sig in
+    */
 
+    createDefaultExperiences(){
+        let experiencias=[
+            {
+                featureImg:"https://picsum.photos/200",
+                featureTitle:"Prueba 1"
+            },
+            {
+                featureImg:"https://picsum.photos/200",
+                featureTitle:"Prueba 2"
+            },
+            {
+                featureImg:"https://picsum.photos/200",
+                featureTitle:"Prueba 3"
+            },
+            {
+                featureImg:"https://picsum.photos/200",
+                featureTitle:"Prueba 4"
+            }
+        ];
 
+        return experiencias;
+    }
     
     
 }
@@ -16,16 +43,9 @@ class View{
         console.log("Vista creada");
     }
 
-    //Funcion que anade al boton la peticion ajax
-    setUpListener(controller){
-        $("#botonPrueba").click(function(){
-            //Funcion del controlador AJAX
-            controller.ajaxRequest();
-        });
-    }
-
-    createDivsExperiences(){
-        
+    //inner text en el div principal con las experiencias preview
+    createDivsExperiences(textHtml){
+        $("#principal").html(textHtml);
     }
 }
 
@@ -33,23 +53,52 @@ class Controller{
     constructor(model,view){
         this.model = model;
         this.view = view;
-        this.view.setUpListener(this);
-        console.log("Controlador creada");
+        //Peticion de ajax al cargar el controlador con las experiencias previas
+        this.ajaxRequestPreviewExperiences();
+        console.log("Controlador creado");
 
     }
 
     //Método para hacer peticiones Ajax contra PHP   
-    ajaxRequest(){
-        $.ajax(
+    ajaxRequestPreviewExperiences(){
+       /* $.ajax(
             {
-                //URL de prueba
-                url: "https://randomuser.me/api/", 
-                success: function(result){
-                    console.log(result);
+                url:"api.php",
+                data:"101",
+                success:function(result){
+                    let textHtml=createPreviewExperiences(result);
+                    view.createDivsExperiences(textHtml);
                 }
             }
-        );
+        );*/
+        let result =this.model.createDefaultExperiences();
+        let textHtml=this.createPreviewExperiences(result);
+        this.view.createDivsExperiences(textHtml);
     }
+
+    ajaxRequestSigIn(){
+        
+    }
+
+    ajaxRequestLogIn(){
+
+    }
+
+    //Forma el texto con las experiencias
+    createPreviewExperiences(arrayExperiences){
+        let textHtml=``;
+        for(let i=0;i<arrayExperiences.length;i++){
+            textHtml+=`<div class="col-md-3">`;
+            textHtml+=`<img src="${arrayExperiences[i].featureImg}" alt="test" > `;
+            textHtml+=`<h2>${arrayExperiences[i].featureTitle}</h2>`;
+            textHtml+=`</div>`;
+        }
+        return textHtml;
+    }
+
+    
+
+
     
 
 }
