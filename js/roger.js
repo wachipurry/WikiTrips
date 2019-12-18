1381bb5c2b4b671609eb1de419c17e2b0b041919
rogercrdaw <a19rogcalrul@inspedralbes.cat>
2019-12-18 03:11:22 +0000

    mas CSS

--- js/roger.js
+++ js/roger.js
@@ -2,10 +2,10 @@ window.onscroll = function() { scrollFunction() };
 
 function scrollFunction() {
     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-        document.getElementById("wt_navbar").style.padding = "20px 10px";
-        document.getElementById("wt_logo").style.fontSize = "20px";
+        document.getElementById("wt_navbar").style.padding = "30px 10px";
+        //document.getElementById("wt_logo").style.fontSize = "20px";
     } else {
         document.getElementById("wt_navbar").style.padding = "80px 10px"; //80px antes
-        document.getElementById("wt_logo").style.fontSize = "5opx";
+        //document.getElementById("wt_logo").style.fontSize = "5opx";
     }
 }
