de57226e158d528a3a99263046e83b09d347bc80
rogercrdaw <a19rogcalrul@inspedralbes.cat>
2019-12-18 23:21:17 +0000

    fulltrip view

---
+++ html/modalFullTrip.php
@@ -0,0 +1,43 @@
+<!-- MODAL OCULTO Y VACIO PARA VISTA DETALADA DE UN TRIP -->
+<div id="viewFullTrip" class="viewFullTrippModal modal fade" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-lg " role="document">
+        <div class="card-body modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title text-warning">TITULO</h5>
+            </div>
+            <div class="modal-body">
+                <p class="text-secondary">Para todos aquellos a los que le gusta sentir el mundo bajo sus pies e ir paso a paso conociendo rincones inaccesibles y vivir la naturaleza de una forma directa y sin barreras esta es tu opción de viaje. Con diferentes niveles pasando del senderismo, accesible a cualquier público medianamente en forma, a los trekkings, sólo aptos para viajeros preparados física y técnicamente, nuestros viajes a pie son una garantía de sentir el mundo como siempre has soñado.</p>
+                <p class="text-secondary text-left">
+                    <a class="text-secondary">Categorias, aventura, a Pie...</a></p>
+                <p class="text-secondary text-left">
+                    <a class="text-secondary">Autor</a><br />
+                    fecha</p>
+
+                <div class="modal-footer">
+                    <form>
+                        <!--ANTES-->
+                        <div class="input-group input-group-sm mt-3 mb-3">
+
+                            <!--selector-->
+                            <select class="form-control-sm" id="sel1">
+                                <option>5</option>
+                                <option>4</option>
+                                <option>3</option>
+                                <option>2</option>
+                                <option>2</option>
+
+                            </select>
+                            <!--BOTONES-->
+                            <div class="input-group-append input-group-sm">
+                                <button class="btn btn-outline-secondary" type="button">VOTAR</button>
+                                <button class="btn btn-outline-warning" type="button" data-dismiss="modal">CERRAR</button>
+
+                            </div>
+                        </div>
+                    </form>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- index_roger.php
+++ index_roger.php
@@ -31,103 +31,17 @@ include('html/modalSignIn.php'); // Modal oculto SigIn
                 <div id="modalAddTripAlert" style="display: none;" role="alert">
 
                 </div>
-                <div class="modal-header">
-                    <h5 class="modal-title text-warning">Comparte con el mundo tus Trips !!</h5>
-                </div>
-                <form>
-                    <div class="modal-body">
-                        <div class="form-group">
-                            <label for="tripTitle">Título</label>
-                            <input type="text" class="form-control" id="tripTitle" placeholder="es un título, no escribas mas de 50 caracteres ;)">
-                        </div>
-                        <div class="form-group">
-                            <label for="tripResume">Resumen</label>
-                            <textarea class="form-control" id="tripResume" rows="2" placeholder="escribe aqui una breve resumen de tu experiència (de 100 a 150 carácteres)"></textarea>
-                        </div>
-                        <div class="form-group">
-                            <label for="tripDescription">Descricónion</label>
-                            <textarea class="form-control" id="tripDescription" rows="5" cols="50" placeholder="cuentanoslo todo !"></textarea>
-                        </div>
-                        <div class="form-group">
-                            <label for="tripLocation">Ubicación</label>
-                            <input type="text" class="form-control" id="tripLocation" placeholder="País, ciudad, ruta, ...">
-                        </div>
-                        <div class="form-group">
-                            <label for="tripCategory">Categoria</label>
-                            <select class="form-control" id="tripCategory">
-                                <option>Love</option>
-                                <option>Adventure</option>
-                                <option>Relax</option>
-                            </select>
-                        </div>
-                        <!--SAQUEMOSNOS DE ENCIMA EL TEMA IMAGEN POR SU COMPLEJIDAD -->
-                        <!--<div class="form-group">
-                    <label for="tripImg">Image</label>
-                    <input type="file" class="form-control" id="tripImg" placeholder="Url of trip image">
-                </div>-->
-                    </div>
-                    <div class="modal-footer">
-                        <button id="submitAddTripButton" type="button" class="btn btn-outline-warning text-secondary">CONFIRMAR</button>
-                        <button id="cancelAddTripButton" type="button" class="btn btn-outline-danger text-secondary">CANCELAR</button>
-                    </div>
-                </form>
+
             </div>
         </div>
     </div>
 
 
-<!-- Creación del modal de prueba con todas las clases de boostrap para que se parezca --
-<div id="viewFullTrip" class="fullTripModal modal fade" tabindex="-1" role="dialog" aria-hidden="true">
+<!-- MODAL OCULTO Y VACIO PARA VISTA DETALADA DE UN TRIP -->
+<!--<div id="viewFullTrip" class="viewFullTrippModal modal fade" tabindex="-1" role="dialog" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="card-body modal-content">
-            <h2>Edit profile</h2>
 
-            <div id="modalFullTripAlert" style="display: none;" role="alert">
-            </div>
-            <div class="modal-header">
-                <h5 class="modal-title text-warning">Ayudanos a saber un poco mas de ti !!</h5>
-            </div>
-            <form>
-                <div class="modal-body">
-
-                    <div class="form-group">
-                        <label for="treatment">Como podemos dirigirnos a ti?</label>
-                        <select class="form-control" id="treatment">
-                            <option>Sr.</option>
-                            <option>Sra.</option>
-                            <option>Don</option>
-                        </select>
-                    </div>
-                    <div class="form-group">
-                        <label for="nameEdit">Nombre</label>
-                        <input type="text" class="form-control" id="nameEdit" placeholder="escribe aquí tu nombre">
-                    </div>
-                    <div class="form-group">
-                        <label for="surnameEdit">Apellido</label>
-                        <input type="text" class="form-control" id="surnameEdit" placeholder="escribe aqui tu apellido">
-                    </div>
-                    <div class="form-group">
-                        <label for="userDescriptionEdit">Cuentanos algo sobre ti</label>
-                        <textarea type="text" class="form-control" rows="4" id="userDescriptionEdit" placeholder="escribe aqui tu apellido">
-                    </div>
-                    <div class="form-group">
-                        <label for="newEmail">Nueva dirección email</label>
-                        <input type="newEmail" class="form-control" id="newEmail" placeholder="hola@wikitrips.cat">
-                    </div>
-                    <div class="form-group">
-                        <label for="publicityEdit">Quieres que te informemos de las ultimas novedades en la web?</label>
-                        <select class="form-control" id="publicityEdit">
-                            <option>Si</option>
-                            <option>No.</option>
-                        </select>
-                    </div>
-
-                </div>
-                <div class="modal-footer">
-                    <button id="submitEditProfileButton" type="button" class="btn btn-outline-warning text-secondary">CONFIRMAR</button>
-                    <button id="cancelEditProfileButton" type="button" class="btn btn-outline-danger text-secondary">CANCELAR</button>
-                </div>
-            </form>
         </div>
     </div>
 </div>-->
