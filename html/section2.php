d6edc77ad35f8bdd3176756c5b363b2101df051d
rogercrdaw <a19rogcalrul@inspedralbes.cat>
2019-12-18 09:10:21 +0100

    merging

--- html/section2.php
+++ html/section2.php
@@ -2,23 +2,6 @@
 <section id="roger_02">
     <div class="container">
         <div class="row">
-            <div class="col-md-3">
-                <label class="btn btn-dark">Filter by:</label>
-            </div>
-            <div class="col-md-3">
-                <button type="button" class="btn btn-outline-dark" >Date</button>
-            </div>
-            <div class="col-md-3">
-                <button type="button" class="btn btn-outline-dark">Rate</button>
-            </div>
-            <div class="col-md-3">
-                <!-- Petición ajax para cargarlas-->
-                <select class="btn btn-outline-dark form-control" id="v">
-                    <option>Love</option>
-                    <option>Adventure</option>
-                    <option>Relax</option>
-                </select>
-            </div>
         </div>
         <p class="text-warning">Faucibus turpis in eu mi bibendum neque. Laoreet non curabitur gravida arcu ac tortor dignissim convallis aenean. Risus in hendrerit gravida rutrum quisque non tellus orci ac. Nunc id cursus metus aliquam eleifend mi in nulla posuere. Fames ac turpis egestas integer. Interdum velit euismod in pellentesque massa placerat duis ultricies. Imperdiet nulla malesuada pellentesque elit eget. Risus in hendrerit gravida rutrum. Pretium viverra suspendisse potenti nullam ac tortor vitae purus faucibus. Quis risus sed vulputate odio ut enim. Est ullamcorper eget nulla facilisi etiam dignissim diam quis. Neque volutpat ac tincidunt vitae semper quis lectus nulla.</p>
 
